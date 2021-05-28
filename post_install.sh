80141080b574388d3cabf211f2f58dbb2d9251bf
kennedyoliveira <kennedyroliveira@gmail.com>
2021-05-28 00:25:59 -0300

    Debug

--- post_install.sh
+++ post_install.sh
@@ -1,15 +1,21 @@
 #!/bin/sh
 
 # Create folder structure
+echo Creating the folder structure
 mkdir -p /usr/local/jdownloader
 
 # download the installer
+echo Downloading the installer
 curl http://installer.jdownloader.org/JDownloader.jar -o /usr/local/jdownloader/JDownloader.jar
 
+ls /usr/local/jdownloader
+
 # Add a new user
+echo Adding new user
 pw useradd jdownloader -u 333 -c "JDownloader daemon user" -d /nonexistent -s /sbin/nologin
 
 # update the folder permissions
+echo "Updating permission"
 chown -R jdownloader:nogroup /usr/local/jdownloader
 
 # enable JDownloader on start
@@ -19,4 +25,5 @@ sysrc jdownloader_enable=YES
 sysrc jdownloader_user=jdownloader
 
 # start the service
+echo "Starting the service"
 service jdownloader start
