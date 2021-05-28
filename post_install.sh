2a2c37917bc9b0e25b9ae521e793cd48d729a664
kennedyoliveira <kennedyroliveira@gmail.com>
2021-05-27 22:42:32 -0300

    Improve post_install script

--- post_install.sh
+++ post_install.sh
@@ -1,20 +1,16 @@
 #!/bin/sh
 
 # Create folder structure
-cd /usr/local
-mkdir jdownloader
-
-cd jdownloader
+mkdir -p /usr/local/jdownloader
 
 # download the installer
-curl http://installer.jdownloader.org/JDownloader.jar -o JDownloader.jar
+curl http://installer.jdownloader.org/JDownloader.jar -o /usr/local/jdownloader/JDownloader.jar
 
 # Add a new user
 pw useradd jdownloader -u 333 -c "JDownloader daemon user" -d /nonexistent -s /sbin/nologin
 
 # update the folder permissions
-cd ..
-chown -R jdownloader:nogroup jdownloader
+chown -R jdownloader:nogroup /usr/local/jdownloader
 
 # enable JDownloader on start
 sysrc jdownloader_enable=YES
@@ -24,4 +20,3 @@ sysrc jdownloader_user=jdownloader
 
 # start the service
 service jdownloader start
-
